20d954e97d5474ed43519ad0099824cbfcf7ff5d
Geunyoung Kim <kgysmart@gmail.com>
2018-12-19 10:23:14 -0800

    Add Template Composition

--- webapp/main.go
+++ webapp/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -37,17 +39,35 @@ func main() {
 	*/
 	// http.ListenAndServe(":8000", http.FileServer(http.Dir("../public")))
 
-	templates := populateTemplates()
+	/*
+		templates := populateTemplates()
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			requestedFile := r.URL.Path[1:]
+			t := templates.Lookup(requestedFile + ".html")
+			if t != nil {
+				err := t.Execute(w, nil)
+				if err != nil {
+					log.Println(err)
+				}
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+			}
+		})
+		http.Handle("/img/", http.FileServer(http.Dir("../public")))
+		http.Handle("/css/", http.FileServer(http.Dir("../public")))
+		http.ListenAndServe(":8000", nil) // http://localhost:8000/home
+	*/
+	tempaltes := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
-		t := templates.Lookup(requestedFile + ".html")
-		if t != nil {
-			err := t.Execute(w, nil)
+		template := tempaltes[requestedFile+".html"]
+		if template != nil {
+			err := template.Execute(w, nil)
 			if err != nil {
 				log.Println(err)
 			}
 		} else {
-			w.WriteHeader(http.StatusNotFound)
+			w.WriteHeader(404)
 		}
 	})
 	http.Handle("/img/", http.FileServer(http.Dir("../public")))
@@ -55,9 +75,49 @@ func main() {
 	http.ListenAndServe(":8000", nil) // http://localhost:8000/home
 }
 
-func populateTemplates() *template.Template {
-	result := template.New("templates")
+// func populateTemplates() *template.Template {
+// 	result := template.New("templates")
+// 	const basePath = "../templates"
+// 	template.Must(result.ParseGlob(basePath + "/*.html"))
+// 	return result
+// }
+
+func populateTemplates() map[string]*template.Template {
+	/*
+		result := template.New("templates")
+		const basePath = "../templates"
+		template.Must(result.ParseGlob(basePath + "/*.html"))
+		return result
+	*/
+
+	result := make(map[string]*template.Template)
 	const basePath = "../templates"
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content/" + fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '" + fi.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
 	return result
 }
