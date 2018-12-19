0c08936ef204decf2524b30c150a45afa2686f67
Geunyoung Kim <kgysmart@gmail.com>
2018-12-19 09:32:54 -0800

    Add Loading Templates

--- webapp/main.go
+++ webapp/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
 	/* // Custom File Server
@@ -31,5 +35,29 @@ func main() {
 		})
 		http.ListenAndServe(":8000", nil)
 	*/
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("../public")))
+	// http.ListenAndServe(":8000", http.FileServer(http.Dir("../public")))
+
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("../public")))
+	http.Handle("/css/", http.FileServer(http.Dir("../public")))
+	http.ListenAndServe(":8000", nil) // http://localhost:8000/home
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "../templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
