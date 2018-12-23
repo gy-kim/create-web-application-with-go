4089d5f818102706b2c604d38e4505a79d185737
Geunyoung Kim <kgysmart@gmail.com>
2018-12-22 21:11:27 -0800

    Add Form Posts

--- webapp/controller/controller.go
+++ webapp/controller/controller.go
@@ -12,6 +12,8 @@ var (
 
 func Startup(templates map[string]*template.Template) {
 	homeController.homeTemplate = templates["home.html"]
+	homeController.standLocatorTemplate = templates["stand_locator.html"]
+	homeController.loginTemplate = templates["login.html"]
 	shopController.shopTemplate = templates["shop.html"]
 	shopController.categoryTemplate = templates["shop_details.html"]
 	homeController.registerRoutes()
